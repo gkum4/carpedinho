d2c3fabe26e14a5137ece8dd1638938bd9eab657
ejwindows <juniorklawa10@gmail.com>
2020-04-27 21:38:55 -0300

    added .prettierrc and fixed container padding

--- src/pages/Timeline/index.js
+++ src/pages/Timeline/index.js
@@ -1,65 +1,68 @@
-import React from 'react';
+import React from "react";
 
-import { View, Text, ImageBackground, FlatList, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
-import backgroundImage from '../../assets/backgroundImage.png';
+import backgroundImage from "../../assets/backgroundImage.png";
 
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon from "react-native-vector-icons/FontAwesome";
 
-import styles from './styles';
+import styles from "./styles";
 
-import { timelineData } from '../../data';
-
-import { useNavigation } from '@react-navigation/native';
+import { timelineData } from "../../data";
 
 const Timeline = () => {
-  const navigation = useNavigation();
-
   const TimelineList = () => {
     return (
       <FlatList
         data={timelineData}
-        keyExtractor={item => item.date}
+        keyExtractor={(item) => item.date}
         ListHeaderComponent={() => (
           <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>
-              Minha Timeline
-            </Text>
+            <Text style={styles.titleText}>Minha Timeline</Text>
           </View>
         )}
-        renderItem={({item}) => (
-          <TouchableOpacity onPress={() => navigation.navigate('Day')}>
+        renderItem={({ item }) => (
+          <TouchableOpacity>
             <View style={styles.listItemTopFeelingColors}>
               {item.colors.map((feeling) => (
-                <View key={feeling} style={{backgroundColor: feeling, flex: 1}} />
+                <View
+                  key={feeling}
+                  style={{ backgroundColor: feeling, flex: 1 }}
+                />
               ))}
             </View>
             <View style={styles.listItemContainer}>
-              <Icon name="star-o" size={20} style={styles.listItemStar}/>
+              <Icon name="star-o" size={20} style={styles.listItemStar} />
               <Text style={styles.listItemText}>{item.date}</Text>
               <View style={styles.listItemIconsContainer}>
-                {item.note && <Icon name="book" size={20} style={{ marginLeft: 10 }} />}
+                {item.note && (
+                  <Icon name="book" size={20} style={{ marginLeft: 10 }} />
+                )}
                 {item.activities && <Icon name="list" size={20} />}
               </View>
             </View>
           </TouchableOpacity>
         )}
-        ItemSeparatorComponent={() => (
-          <View style={styles.listItemBar}/>
-        )}
+        ItemSeparatorComponent={() => <View style={styles.listItemBar} />}
       />
     );
-  }
+  };
 
   return (
-    <View style={styles.container}>
+    <>
       <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
-
-        <TimelineList />
-
+        <View style={styles.container}>
+          <TimelineList />
+        </View>
       </ImageBackground>
-    </View>
+    </>
   );
-}
+};
 
 export default Timeline;
